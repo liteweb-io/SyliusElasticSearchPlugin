5d4066b7af31142df7bbef1f0a87d9a7abee26bf
Kamil Fronczak <axotion@linux.pl>
2018-06-11 15:46:00 +0200

    Update VariantDocument.php

--- src/Document/VariantDocument.php
+++ src/Document/VariantDocument.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace Sylius\ElasticSearchPlugin\Document;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use ONGR\ElasticsearchBundle\Annotation as ElasticSearch;
-use ONGR\ElasticsearchBundle\Collection\Collection;
+use Doctrine\Common\Collections\Collection;
 
 /**
  * @ElasticSearch\Nested
@@ -70,8 +71,8 @@ class VariantDocument
 
     public function __construct()
     {
-        $this->images = new Collection();
-        $this->options = new Collection();
+        $this->images = new ArrayCollection();
+        $this->options = new ArrayCollection();
     }
 
     /**
