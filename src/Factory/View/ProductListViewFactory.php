4642698409e462e1701368357a7f6e0a11f8f78b
Kamil Fronczak <axotion@linux.pl>
2018-06-22 13:34:49 +0200

    Update ProductListViewFactory.php

--- src/Factory/View/ProductListViewFactory.php
+++ src/Factory/View/ProductListViewFactory.php
@@ -214,6 +214,7 @@ final class ProductListViewFactory implements ProductListViewFactoryInterface
         $productView->variants = $this->getVariantViews($product->getVariants());
         $productView->price = $this->getPriceView($product->getPrice());
         $productView->createdAt = $product->getCreatedAt();
+        $productView->in_magazine = $product->getInMagazine();
 
         return $productView;
     }
