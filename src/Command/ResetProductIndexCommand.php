ef15cf44ae9fb0c44f05d107ec496e364c2b89e1
tomaszfelczyk <tomasz.felczyk@liteweb.io>
2018-11-20 21:57:47 +0100

    dump prod fail

--- src/Command/ResetProductIndexCommand.php
+++ src/Command/ResetProductIndexCommand.php
@@ -104,9 +104,7 @@ final class ResetProductIndexCommand extends Command
                                 $channel
                             );
                         }
-
-
-                        dump($productDocument->getId());
+                        
                         $this->elasticsearchManager->persist($productDocument);
 
                         ++$productDocumentsCreated;
